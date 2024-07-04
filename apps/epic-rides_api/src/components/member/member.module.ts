9242aa7d264f1efd85a242b794a382889fac38b2
Bobur98 <bmuhibullaev@mail.ru>
2024-07-05 02:50:31 +0900

    fix: modify member module & member service

--- apps/epic-rides_api/src/components/member/member.module.ts
+++ apps/epic-rides_api/src/components/member/member.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
+import MemberSchema from '../../schemas/Member.model';
 
 @Module({
-  providers: [MemberResolver, MemberService]
+	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }])], // to use Schema model in service model, we need to import it module file
+	providers: [MemberResolver, MemberService],
 })
 export class MemberModule {}
