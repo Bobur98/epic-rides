3328d0d10c63d9a6c89776e6a4c23d20d50f4ba0
Bobur98 <bmuhibullaev@mail.ru>
2024-07-05 02:19:07 +0900

    feat: create components based modules

--- apps/epic-rides_api/src/components/components.module.ts
+++ apps/epic-rides_api/src/components/components.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-	imports: [MemberModule, ProductModule],
+	imports: [MemberModule, ProductModule, AuthModule, CommentModule, LikeModule, ViewModule, FollowModule, BoardArticleModule],
 })
 export class ComponentsModule {}
