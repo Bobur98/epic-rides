193126173b27733513568f2282d58b4e2984d7b4
Bobur98 <bmuhibullaev@mail.ru>
2024-07-08 01:27:00 +0900

    feat: develop single and multi  imageUploader graphQL apis

--- apps/epic-rides_api/src/libs/config.ts
+++ apps/epic-rides_api/src/libs/config.ts
@@ -2,7 +2,17 @@ import { ObjectId } from 'bson';
 
 export const availableAgentSorts = ['createdAt, updatedAt, memberViews, memberLikes, memberRank'];
 export const availableMemberSorts = ['createdAt, updatedAt, memberViews, memberLikes'];
+// IMAGE CONFIGURATION (config.js)
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+import { T } from './types/common';
+import { pipeline } from 'stream';
 
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+};
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
