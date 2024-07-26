5e2a14eb2e708a3424b3e4c1035aa72283a77ece
Bobur98 <bmuhibullaev@mail.ru>
2024-07-27 00:13:00 +0900

    fix: modify FaqType

--- apps/epic-rides_api/src/libs/enums/faq.enum.ts
+++ apps/epic-rides_api/src/libs/enums/faq.enum.ts
@@ -3,11 +3,11 @@ import { registerEnumType } from '@nestjs/graphql';
 export enum FaqType {
 	MOTORCYCLE = 'Motorcycle',
 	PAYMENT = 'Payment',
-	BUYERS = 'For Buyers',
+	BUYERS = 'Buyers',
 	MAKLERS = 'Maklers',
 	MEMBERSHIP = 'Membership',
 	COMMUNITY = 'Community',
-	OTHER = 'Other Inquiries',
+	OTHER = 'Other',
 }
 registerEnumType(FaqType, {
 	name: 'FaqType',
